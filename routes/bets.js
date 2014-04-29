85216b77aff78fa1216ca417963cdcb345f86ee5
Karthik Bala <drmelonhead@gmail.com>
2014-04-28 17:17:06 -0700

    it worksgit add .git add .git add .git add .git add .

--- routes/bets.js
+++ routes/bets.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/new', function(req, res){
-	res.render('newbet');
+	res.render('newbet', { message : req.flash('betMessage') });
 });
 
 
@@ -24,15 +24,21 @@ router.post('/new', function(req, res) {
 	bet.description = req.body.des;
 	User.findOne({ id: req.session.user_id }, function(err, user){
 		if(err) throw err;
+
 		if(!user){
 			bet.created_by = 'anon';
 			res.send('o god what the fuck');
 		}
 		bet.created_by = user.username;
-	});
-	bet.save(function(err){
-		if(err) throw err;
-		res.redirect('/bets/' + bet.id);
+		bet.save(function(err){
+			if(err){
+				if(err.code == '11000'){
+					req.flash('betMessage', 'That name is taken, sorry.');
+					return res.redirect('/bets/new');
+				}
+			}
+			res.redirect('/bets/' + bet.id);
+		});
 	});
 });
 
@@ -44,8 +50,8 @@ router.get('/:id', function(req, res){
 			res.send('Bet could not be found');
 			return;
 		}
-		console.log('bet request ' + bet.name);
-		res.render('bet', {'bet' : bet}); //  problem here
+		console.log('bet request ' + bet);
+		res.render('bet', {'bet' : bet}); 
 	});
 });
 
