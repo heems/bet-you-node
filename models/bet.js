9c3c87d4f253819ff1026de0e5190fd1f0117ed6
Karthik Bala <drmelonhead@gmail.com>
2014-05-23 20:06:40 -0700

    started ui

--- models/bet.js
+++ models/bet.js
@@ -4,7 +4,9 @@ var Schema = mongoose.Schema;
 var BetSchema = new Schema({
 	name: {type: String, required: true, index: {unique: true}},
 	description: {type: String, required: true},
-	created_by: {type: String, requried: true}
+	created_by: {type: String, requried: true},
+	submissions: [String]
 });
+
 
 module.exports = mongoose.model('Bet', BetSchema);
